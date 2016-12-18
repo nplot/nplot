37ddc9c1bcdea2a2ef4c31655f21a0d7f2a4208e
Jonas Stein <news@jonasstein.de>
2016-12-18 20:51:38 +0100

    Change formatting in haeder for doc('nplot')

--- Qvert.m
+++ Qvert.m
@@ -1,7 +1,5 @@
 function Qv = Qvert(scan, nomean)
-
-% for FLATCONE:
-% Determine vertical component of momentum transfer
+% for FLATCONE: Determine vertical component of momentum transfer
 % If nomean, no averaging done
 %
 % P. Steffens 07/2008
@@ -15,15 +13,16 @@ ki = getvar(scan,'ki');
 Qv = cos(delta).*sin(gamma).*ki;
 
 if nargin<2 || ~nomean  % Give average unless nomean is set
-    if (max(Qv)-min(Qv)) < maxdeviate.QVERT %all equal within accuracy
+    if (max(Qv)-min(Qv)) < maxdeviate.QVERT % all equal within accuracy
         Qv = mean(Qv);
     else
-        fprintf('Warning: inconsitency in vertical component of Scan %s  during call to Qvert.\n', scan.FILE);
+        fprintf('Warning: Inconsitency in vertical component of Scan %s  during call to Qvert.\n', scan.FILE);
     end
 end
-
 
 if QSign == -1 
     Qv = -Qv;
     %(as it has before been calculated for Q=kf-ki)
 end
+
+end %end Qvert
