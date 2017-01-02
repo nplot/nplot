fb521a3bddd2864ff76a4b5ba4729eba48a5fd45
Jonas Stein <news@jonasstein.de>
2017-01-02 23:12:43 +0100

    Add option textpm for plain text +/-, add helptext

--- converterror.m
+++ converterror.m
@@ -1,18 +1,37 @@
 function errstr = converterror(value,err,opt,prec)
-
-% Format Value and Error into a single string
-% if opt='pm' then format with "+/-",else with brackets
+% CONVERTERROR formats value and error into a single string
 % prec for additional precision (e.g. prec = 2 for the two first
 % significant digits), default: 1
-
+%
+% Usage: 
+%   sprintf('%s',converterror(12.123456,0.004321,'pm',3))
+%    12.12346 \pm 0.00432
+%
+%   sprintf('%s',converterror(12.123456,0.004321,'textpm',3))
+%    12.12346 +/- 0.00432
+%
+%   sprintf('%s',converterror(12.123456,0.004321,'()',3))
+%    12.1235(432)
+%
 % P. Steffens, 09/2012
+% J. Stein, 01/2017
 
-pm = nargin>2 && strcmpi(opt,'pm');
+
+if nargin>2 && strcmpi(opt,'textpm')
+    pmsymbol = '+/-';
+    pm = true;
+elseif nargin>2 && strcmpi(opt,'pm')
+    pmsymbol = '\pm';
+    pm = true;
+else
+    pm = false;
+end
+    
 if nargin<4, prec = 1; end
 
 if err<=0
     errstr = num2str(value);
-    if pm, errstr = [errstr, ' \pm 0']; end
+    if pm, errstr = [errstr, ' ',pmsymbol,' 0']; end
     return;
 end
 
@@ -23,18 +42,13 @@ if round(err / 10^pos)==1, pos = pos - 1; end
 
 pos = min([pos,0]);  % Never round before decimal point
 
-%     rerr = round(err/10^(pos-1));    
-%     rval = round(value/10^(pos-1))  * 10^(pos-1); 
-%     if pm, rerr = rerr * 10^(pos-1); end
-% else
-    rerr = round(err/10^pos);
-    rval = round(value/10^pos)  * 10^pos;
-    if pm, rerr = rerr * 10^pos; end
-% end
+rerr = round(err/10^pos);
+rval = round(value/10^pos)  * 10^pos;
+if pm, rerr = rerr * 10^pos; end
 
 if pm
     if pos<0, fstring = ['%0.' num2str(abs(pos)) 'f']; else fstring = '%d'; end
-    errstr = [num2str(rval,fstring),' \pm ', num2str(rerr,fstring)];
+    errstr = [num2str(rval,fstring),' ',pmsymbol,' ', num2str(rerr,fstring)];
 else
     errstr = [num2str(rval) '(' num2str(rerr) ')'];
 end
